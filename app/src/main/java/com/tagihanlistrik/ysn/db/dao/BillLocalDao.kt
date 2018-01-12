fb1f78075591aa244ed7d59dd4ed47caace409dd
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-01-12 08:20:46 +0700

    Create a BillLocalDao

---
+++ app/src/main/java/com/tagihanlistrik/ysn/db/dao/BillLocalDao.kt
@@ -0,0 +1,28 @@
+package com.tagihanlistrik.ysn.db.dao
+
+import android.arch.persistence.room.*
+import com.tagihanlistrik.ysn.db.entity.BillLocal
+
+/**
+ * Created by yudisetiawan on 1/11/18.
+ */
+
+@Dao
+interface BillLocalDao {
+
+    @Query("select * from bill_local")
+    fun getAllBillsLocal(): List<BillLocal>
+
+    @Query("select * from bill_local where period = :period")
+    fun getBillLocalByPeriod(period: String): BillLocal
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertBillLocal(billLocal: BillLocal)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun updateBillLocal(billLocal: BillLocal)
+
+    @Delete
+    fun deleteBillLocal(billLocal: BillLocal)
+
+}
