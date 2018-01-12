9e83b7160ca2dfc803544f403d74964a090d301d
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-01-13 01:52:22 +0700

    Create layout to show bill info after check the bill

---
+++ app/src/main/java/com/tagihanlistrik/ysn/views/ui/billinfo/BillBottomSheetDialogFragment.kt
@@ -0,0 +1,16 @@
+package com.tagihanlistrik.ysn.views.ui.billinfo
+
+import android.app.Dialog
+import android.support.design.widget.BottomSheetDialogFragment
+
+/**
+ * Created by yudisetiawan on 1/13/18.
+ */
+class BillBottomSheetDialogFragment : BottomSheetDialogFragment() {
+
+    val TAG = javaClass.simpleName
+
+    override fun setupDialog(dialog: Dialog?, style: Int) {
+        // TODO: do something in here
+    }
+}
